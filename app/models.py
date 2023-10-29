97a76ffba44c8e8e2931fd7d9333d71d575328ae
Kourosh <bakhtiarifard@gmail.com>
2023-10-29 08:13:11 +0330

    added 'User' and 'edit_user' Pages and their routes and Updated Routes and DB Templated '_post' view, 'model'

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from hashlib import md5
+
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -11,6 +13,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -21,6 +25,11 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         # return '<User {}>'.format(self.username)
         return f'<User {self.username}>'
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 
 class Post(db.Model):
